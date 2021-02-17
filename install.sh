694c81d584052ebeee480c57911c95a082f6c7de
avtobys <48291601+avtobys@users.noreply.github.com>
2021-02-17 21:44:59 +0300

    fix

--- install.sh
+++ install.sh
@@ -23,15 +23,17 @@ install_telegram() {
     SET_CHAT_ID="Telegram chat id set: $CHAT_ID"
     TG_URL="https://api.telegram.org/bot$BOT_ID/sendMessage"
     printf "$SET_CHAT_ID\n\n"
-    if [ `command -v ip` ]; then
-         IP=$(ip route get 8.8.8.8 | grep src | awk '{print $NF}')
+    if [ $(command -v ip) ]; then
+        IP=$(ip route get 8.8.8.8 | grep src | awk '{print $NF}')
     fi
     N=$'\n'
-    TG_RES=$(curl -s -X POST --data "chat_id=$CHAT_ID&parse_mode=html&text=$IP $N Settings are almost ready $N $SET_AVG_MAX $N $SET_BOT_ID $N $SET_CHAT_ID" $TG_URL | sed -r 's/.*"ok"[[:space:]]*:[[:space:]]*(true)[[:space:]]*,[[:space:]]*"result".*/\1/')
+    TG_RES=$(curl -s -X POST \
+        --data "chat_id=$CHAT_ID&parse_mode=html&text=$IP $N Settings are almost ready $N $SET_AVG_MAX $N $SET_BOT_ID $N $SET_CHAT_ID" $TG_URL |
+        sed -r 's/.*"ok"[[:space:]]*:[[:space:]]*(true)[[:space:]]*,[[:space:]]*"result".*/\1/')
     if [ "$TG_RES" != "true" ]; then
         printf "\033[0;31mTelegram bot token or chat id is wrong...\033[0m\n"
         install_telegram
-    fi  
+    fi
 }
 
 install_la
@@ -40,11 +42,16 @@ echo -n 'Enter your pastebin.com api developer key(optional): '
 read PASTE_KEY
 
 IFS=""
-SCRIPT=$(curl -s https://raw.githubusercontent.com/avtobys/loadavg-telegram-watcher/main/loadavg_watcher \
-| sed -r "s/AVG_MAX=([0-9]+)/AVG_MAX=$AVG_MAX/" \
-| sed -r "s/BOT_ID=\"[^\"]+\"/BOT_ID=\"$BOT_ID\"/" \
-| sed -r "s/CHAT_ID=\"[^\"]+\"/CHAT_ID=\"$CHAT_ID\"/" \
-| sed -r "s/PASTE_KEY=\"[^\"]+\"/PASTE_KEY=\"$PASTE_KEY\"/")
+SCRIPT=$(curl -s https://raw.githubusercontent.com/avtobys/loadavg-telegram-watcher/main/loadavg_watcher |
+    sed -r "s/AVG_MAX=([0-9]+)/AVG_MAX=$AVG_MAX/" |
+    sed -r "s/BOT_ID=\"[^\"]+\"/BOT_ID=\"$BOT_ID\"/" |
+    sed -r "s/CHAT_ID=\"[^\"]+\"/CHAT_ID=\"$CHAT_ID\"/" |
+    sed -r "s/PASTE_KEY=\"[^\"]+\"/PASTE_KEY=\"$PASTE_KEY\"/")
+
+if [ "$(echo $SCRIPT | sed -n 's/.*\(successful installation marker\).*/\1/ip;T;q')" != "successful installation marker" ]; then
+    printf "\033[0;31mCan't load script https://raw.githubusercontent.com/avtobys/loadavg-telegram-watcher/main/loadavg_watcher\033[0m\n"
+    exit 1
+fi
 
 echo $SCRIPT
 
